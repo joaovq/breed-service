cbd1024ab45aa714f4a073476b4d26fe92ae77ce
joaovq <vitorsnta90@gmail.com>
2023-06-26 15:52:32 -0300

    Initial commit

---
+++ src/main/kotlin/com/joaovq/breedservice/BreedServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.joaovq.breedservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BreedServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BreedServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/joaovq/breedservice/BreedServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joaovq.breedservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BreedServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
